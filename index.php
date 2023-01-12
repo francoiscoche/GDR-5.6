26453d9ab9afdf2a4127bbe672ddf769de3261e6
francois <francois.coche@outlook.it>
2023-01-12 16:28:53 +0100

    ➕add bootstrap + style files and links

--- index.php
+++ index.php
@@ -46,11 +46,12 @@ $content = (!empty($_GET['content'])) ? gdrcd_filter('include', $_GET['content']
     <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- IE9: mi stai ampiamente rompendo i maroni. -->
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <link rel="shortcut icon" href="imgs/favicon.ico" type="image/png"/>
-        <link rel="stylesheet" href="themes/homepage/<?= $PARAMETERS['themes']['homepage']; ?>/homepage.css"
-              type="text/css"/>
+        <link rel="stylesheet" href="themes/homepage/<?= $PARAMETERS['themes']['homepage']; ?>/homepage.css" type="text/css"/>
+        <link rel="stylesheet" href="themes/homepage/<?= $PARAMETERS['themes']['homepage']; ?>/homepage-custom.css" type="text/css"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css"/>
         <title>
             <?php echo $PARAMETERS['info']['site_name']; ?>
